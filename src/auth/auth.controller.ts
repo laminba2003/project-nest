7c6a5b6f1fb863312de888cbf5b8d4fb30052d4d
mamadou lamine ba <laminba2003@gmail.com>
2020-07-19 10:40:44 +0100

    updating the swagger documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,30 +2,32 @@ import { Controller, Get, UseGuards, Post, Request, Body } from '@nestjs/common'
 import { LoginAuthGuard } from './login-auth.guard';
 import { AuthService } from './auth.service';
 import { User } from '../users/user';
-import { ApiTags, ApiResponse, ApiConsumes, ApiBody, ApiProduces } from '@nestjs/swagger';
+import { ApiTags, ApiResponse, ApiConsumes, ApiBody, ApiProduces, PickType, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('auth')
 @Controller()
 export class AuthController {
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @UseGuards(LoginAuthGuard)
   @Post('auth/login')
-  @ApiResponse({ status: 201, description: 'Login successful.'})
-  @ApiResponse({ status: 401, description: 'Unauthorized.'})
+  @ApiOperation({ summary: 'Authenticate the user in order to generate an access token' })
+  @ApiResponse({ status: 201, description: 'Login successful.' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   @ApiConsumes('application/json')
   @ApiProduces('application/json')
   @ApiBody({
-    description: 'The request with the user credentials sent in the payload.',
-    type: Request,
+    description: 'the user credentials sent in the payload.',
+    type: class Login extends PickType(User, ['username', 'password']) { },
   })
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
 
   @Post('auth/register')
-  @ApiResponse({ status: 201, description: 'The user has been successfully registered.'})
+  @ApiOperation({ summary: 'Register the user' })
+  @ApiResponse({ status: 201, description: 'The user has been successfully registered.', type : User })
   @ApiResponse({ status: 409, description: 'The user is already registered.'})
   @ApiConsumes('application/json')
   @ApiBody({
--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { Book } from './book';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { ApiTags, ApiResponse, ApiConsumes, ApiBody, ApiProduces, ApiHeader } from '@nestjs/swagger';
+import { ApiTags, ApiResponse, ApiConsumes, ApiBody, ApiProduces, ApiHeader, ApiOperation } from '@nestjs/swagger';
 
 @ApiTags('books')
 @Controller('books')
@@ -11,19 +11,21 @@ export class BooksController {
     constructor(private service : BooksService) {}
 
     @Get()
-    @ApiResponse({ status: 200, description: 'The public books has been successfully retrieved.'})
+    @ApiOperation({ summary: 'Retrieve the list of public books' })
+    @ApiResponse({ status: 200, description: 'The public books has been successfully retrieved.', type : [Book] })
     @ApiProduces('application/json')
     async getPublicBooks(): Promise<Book[]> {
         return this.service.getPublicBooks();
     }
 
     @UseGuards(JwtAuthGuard)
+    @Get("private")
+    @ApiOperation({ summary: 'Retrieve the list of private books' })
     @ApiHeader({
         name: 'Authorization',
         description: 'The Bearer JWT token.',
     })
-    @Get("private")
-    @ApiResponse({ status: 200, description: 'The private books has been successfully retrieved.'})
+    @ApiResponse({ status: 200, description: 'The private books has been successfully retrieved.', type : [Book]})
     @ApiResponse({ status: 401, description: 'Unauthorized.'})
     @ApiProduces('application/json')
     async getPrivateBooks(): Promise<Book[]> {
@@ -32,11 +34,16 @@ export class BooksController {
 
     @UseGuards(JwtAuthGuard)
     @Post()
+    @ApiOperation({ summary: 'Create a book' })
     @ApiHeader({
         name: 'Authorization',
         description: 'The Bearer JWT token.',
     })
-    @ApiResponse({ status: 201, description: 'The book has been successfully created.'})
+    @ApiHeader({
+        name: 'Authorization',
+        description: 'The Bearer JWT token.',
+    })
+    @ApiResponse({ status: 201, description: 'The book has been successfully created.', type : Book})
     @ApiResponse({ status: 401, description: 'Unauthorized.'})
     @ApiResponse({ status: 500, description: 'Validation failed.'})
     @ApiConsumes('application/json')
